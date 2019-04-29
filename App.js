9ea401d456e9eaca4eca44a416982275d993fd13
Jose Cerqueira <jose@origensmedia.com>
2019-04-29 18:40:26 +0100

    feat: added react-navigation

--- App.js
+++ App.js
@@ -1,19 +1,81 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Platform } from "react-native";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
 import AddEntry from "./components/AddEntry";
 import History from "./components/History";
+import {
+	createBottomTabNavigator,
+	createAppContainer,
+	createMaterialTopTabNavigator
+} from "react-navigation";
+import { purple, white } from "./utils/colors";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
 
-export default class App extends React.Component {
+class App extends React.Component {
 	render() {
 		return (
 			<Provider store={createStore(reducer)}>
 				<View style={{ flex: 1 }}>
-					<History />
+					<AppContainer />
 				</View>
 			</Provider>
 		);
 	}
 }
+
+const RouteConfigutation = {
+	History: {
+		screen: History,
+		navigationOptions: {
+			tabBarLabel: "History",
+			tabBarIcon: ({ tintColor }) => (
+				<Ionicons name="ios-bookmarks" size={30} color={tintColor} />
+			)
+		}
+	},
+	addEntry: {
+		screen: AddEntry,
+		navigationOptions: {
+			tabBarLabel: "Add Entry",
+			tabBarIcon: ({ tintColor }) => (
+				<FontAwesome name="plus-square" size={30} color={tintColor} />
+			)
+		}
+	}
+};
+
+const TabNavigatorConfig = {
+	navigationOptions: {
+		header: null
+	},
+	tabBarOptions: {
+		activeTintColor: Platform.OS === "ios" ? purple : white,
+		style: {
+			height: 56,
+			backgroundColor: Platform.OS === "ios" ? white : purple,
+			shadowColor: "rgba(0,0,0,0.24)",
+			shadowOffset: {
+				width: 0,
+				height: 3
+			},
+			shadowRadius: 6,
+			shadowOpacity: 1
+		}
+	}
+};
+
+const AppContainer =
+	Platform.OS === "ios"
+		? createAppContainer(
+				createBottomTabNavigator(RouteConfigutation, TabNavigatorConfig)
+		  )
+		: createAppContainer(
+				createMaterialTopTabNavigator(
+					RouteConfigutation,
+					TabNavigatorConfig
+				)
+		  );
+
+export default App;
