c58a64638518b5f6c075b78b6a0cefde8d455893
Jose Cerqueira <jose@origensmedia.com>
2019-04-30 01:23:38 +0100

    Feat: createStackNavigator and EntryDetail added

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Platform } from "react-native";
+import { View, Platform, StatusBar } from "react-native";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
@@ -8,21 +8,40 @@ import History from "./components/History";
 import {
 	createBottomTabNavigator,
 	createAppContainer,
-	createMaterialTopTabNavigator
+	createMaterialTopTabNavigator,
+	createStackNavigator
 } from "react-navigation";
 import { purple, white } from "./utils/colors";
 import { FontAwesome, Ionicons } from "@expo/vector-icons";
+import { Constants } from "expo";
+import EntryDetail from "./components/EntryDetail";
 
 class App extends React.Component {
 	render() {
 		return (
 			<Provider store={createStore(reducer)}>
 				<View style={{ flex: 1 }}>
-					<AppContainer />
+					<UdacityStatusBar
+						backgroundColor={purple}
+						barStyle="light-content"
+					/>
+					<MainNavigator />
 				</View>
 			</Provider>
 		);
 	}
+}
+
+function UdacityStatusBar({ backgroundColor, ...props }) {
+	return (
+		<View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+			<StatusBar
+				translucent
+				backgroundColor={backgroundColor}
+				{...props}
+			/>
+		</View>
+	);
 }
 
 const RouteConfigutation = {
@@ -68,14 +87,24 @@ const TabNavigatorConfig = {
 
 const AppContainer =
 	Platform.OS === "ios"
-		? createAppContainer(
-				createBottomTabNavigator(RouteConfigutation, TabNavigatorConfig)
-		  )
-		: createAppContainer(
-				createMaterialTopTabNavigator(
-					RouteConfigutation,
-					TabNavigatorConfig
-				)
-		  );
+		? createBottomTabNavigator(RouteConfigutation, TabNavigatorConfig)
+		: createMaterialTopTabNavigator(RouteConfigutation, TabNavigatorConfig);
+
+const MainNavigator = createAppContainer(
+	createStackNavigator({
+		Home: {
+			screen: AppContainer
+		},
+		EntryDetail: {
+			screen: EntryDetail,
+			navigationOptions: {
+				headerTintColor: white,
+				headerStyle: {
+					backgroundColor: purple
+				}
+			}
+		}
+	})
+);
 
 export default App;
